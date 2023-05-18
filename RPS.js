747b407349f65d76312ad89cdc2b6a3fc977579d
Liangchou Yu <liangchou.leo.yu@gmail.com>
2023-05-18 13:45:18 -0400

    fix logic

--- RPS.js
+++ RPS.js
@@ -61,6 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function determineScore(playerChoice, computerChoice) {
         if (playerChoice === computerChoice) {
+            return 'tie'
         }
 
         if (playerChoice === "rock") {
@@ -156,9 +157,108 @@ document.addEventListener('DOMContentLoaded', function() {
         // display player choices
         playerChoices.forEach(icon => {
             icon.style.display = 'block';
-
         });
-
-
     })
 });
+
+
+/*
+gpt 'cleaner' codes 
+
+document.addEventListener('DOMContentLoaded', function() {
+  const playerChoices = document.querySelectorAll('.playerChoices > div');
+  const computerChoices = document.querySelectorAll('.computerChoices > div');
+  const resetBtn = document.getElementById('resetBtn');
+  let playerScore = 0;
+  let computerScore = 0;
+
+  const playerScorevalue = document.querySelector('.player-scorevalue');
+  const computerScorevalue = document.querySelector('.computer-scorevalue');
+  const playerScoreboard = document.querySelector('.player-scoreboard');
+  const computerScoreboard = document.querySelector('.computer-scoreboard');
+
+  playerChoices.forEach((choice, index) => {
+    choice.addEventListener('click', function() {
+      const choices = ['rock', 'paper', 'scissors'];
+      const playerChoice = choices[index];
+      const computerChoice = getComputerChoice();
+      determineScore(playerChoice, computerChoice);
+      determineWinner(playerScore, computerScore);
+    });
+  });
+
+  function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const computerChoice = choices[randomIndex];
+
+    const computerChoiceElement = document.querySelector(`#bot-${computerChoice}`);
+    computerChoiceElement.classList.add('active');
+
+    setTimeout(() => {
+      computerChoiceElement.classList.remove('active');
+    }, 1000);
+
+    return computerChoice;
+  }
+
+  function determineScore(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return;
+    }
+
+    if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      playerScore++;
+      playerScorevalue.textContent = playerScore;
+    } else {
+      computerScore++;
+      computerScorevalue.textContent = computerScore;
+    }
+  }
+
+  function determineWinner(playerScore, computerScore) {
+    if (playerScore === 5 || computerScore === 5) {
+      playerChoices.forEach(icon => {
+        icon.style.display = 'none';
+      });
+
+      resetBtn.style.display = 'block';
+
+      if (playerScore === 5) {
+        playerScoreboard.classList.add('active');
+        playerScorevalue.classList.add('active');
+        return 'Player reaches 5, player wins';
+      } else {
+        computerScoreboard.classList.add('active');
+        computerScorevalue.classList.add('active');
+        return 'Computer reaches 5, computer wins';
+      }
+    }
+  }
+
+  resetBtn.addEventListener('click', function() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreboard.classList.remove('active');
+    playerScorevalue.classList.remove('active');
+    computerScoreboard.classList.remove('active');
+    computerScorevalue.classList.remove('active');
+    playerScorevalue.textContent = '0';
+    computerScorevalue.textContent = '0';
+    resetBtn.style.display = 'none';
+    playerChoices.forEach(icon => {
+      icon.style.display = 'block';
+    });
+  });
+});
+
+1. Used the forEach loop to iterate over player choices and attach event listeners, reducing the need for an explicit for loop.
+2. Simplified the scoring logic by using a conditional statement to check if the player wins.
+3. Combined the element selections for player score and computer score into a single querySelector or querySelectorAll call, improving code readability and performance.
+
+
+*/
